fcecfff103a1eda24a1f2c3b88d636431513f85b
João Gabriel <jgabriiell33@gmail.com>
2024-06-11 10:57:45 -0300

    creating project.

---
+++ src/main/java/me/dio/santander_bootcamp_2024/SantanderBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcamp2024Application.class, args);
+	}
+
+}
