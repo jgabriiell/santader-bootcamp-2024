0bad39b630bb767aaeba457255903c50e2c458c2
João Gabriel <jgabriiell33@gmail.com>
2024-06-11 12:00:44 -0300

    Domain layer implemented

--- src/main/java/me/dio/domain/Application.java
+++ src/main/java/me/dio/domain/Application.java
@@ -1,13 +1,13 @@
-package me.dio.santander_bootcamp_2024;
+package me.dio.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderBootcamp2024Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBootcamp2024Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
